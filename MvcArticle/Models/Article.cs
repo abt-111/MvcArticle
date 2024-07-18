286d7c8fd96646d494f7b3d949d2365b5a19ff86
abt-111 <anthony.temmel@gmail.com>
2024-07-18 15:13:15 +0200

    Ajout de la classe Article dans le dossier Models

---
+++ MvcArticle/Models/Article.cs
@@ -0,0 +1,9 @@
+﻿namespace MvcArticle.Models
+{
+    public class Article
+    {
+        public int Id { get; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
